026baa1943fadd7ecfed120ab43d3ef84e8dcc0f
sahani02 <2002sahani@gmail.com>
2025-05-31 20:43:47 +0530

    Update d13.c

--- DAY 13/d13.c
+++ DAY 13/d13.c
@@ -286,21 +286,25 @@ Accepts two strings as input from the user.
 Concatenates the two strings Displays the concatenated result.
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char ch;
+    char str1[100], str2[100];
 
-    printf("Enter first string (end with #): ");
-    while ((ch = getchar()) != '#') {
-        putchar(ch);
-    }
+    // Get input from user
+    printf("Enter the first string: ");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = '\0';  // Remove newline character
 
-    printf("Enter second string (end with #): ");
-    while ((ch = getchar()) != '#') {
-        putchar(ch);
-    }
+    printf("Enter the second string: ");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = '\0';  // Remove newline character
 
-    printf("\n");
+    // Concatenate the strings
+    strcat(str1, str2);
+
+    // Display the result
+    printf("Concatenated string: %s\n", str1);
 
     return 0;
 }
