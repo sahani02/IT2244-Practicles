11f78878e6077afc21cfbb2da76d66121098c4f8
sahani02 <2002sahani@gmail.com>
2025-05-31 21:21:27 +0530

    Update new 1.c

--- DAY 14/new 1.c
+++ DAY 14/new 1.c
@@ -18,10 +18,10 @@ int main(){
 	
 int f=fork();
 if(f==0){
-	printf("i am child %d\n", );
+	printf("i am child %d\n",getpid() );
 }
 else{
-	printf("I am perent %d\n");
+	printf("I am perent %d\n",getpid());
 }
 return 0;
 }
@@ -35,14 +35,14 @@ int main(){
 int f1=fork();
 int f2=fork();
 if(f1==0){
-	printf("i am child \n");
+	printf("I am child %d\n",getpid());
 	
 }
 else if(f2==0){
-	printf("I am sibling \n");
+	printf("I am sibling %d\n",getpid());
 }
 else{
-	printf("I am parent \n");
+	printf("I am parent %d\n",getpid());
 }
 return 0;
 }
@@ -53,7 +53,7 @@ int main(){
 
 	int f=fork();
 	if(f==0){
-			printf("i am child. my id  \n", getppid(),getpid());
+			printf("i am child. my id  %d my parent id %d\n", getppid(),getpid());
 	}
 	else{
 			int f1=fork();
@@ -61,7 +61,7 @@ int main(){
 			  printf("i am sibiling. my parent id %d My id is %d  \n", getppid(),getpid());
 			}
 			else{
-					printf("i am parent. my parent id is %d my id is  \n", getppid(),getpid());
+					printf("i am parent. my parent id is %d my id is  %d\n", getppid(),getpid());
 			}
 	}
 return 0;
