ab4c36054d9ec7287b87730bf9cd544374e8662a
sahani02 <2002sahani@gmail.com>
2025-06-06 14:21:50 +0530

    Add files via upload

---
+++ DAY 20/pipe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MSGSIZE 16
+char* msg1="hello,world #1";
+char* msg2="hello,world #2";
+char* msg3="hello,world #3";
+
+int main(){
+	char inbuf[MSGSIZE]; //[]
+	int p[2], i;
+	
+	if(pipe(p)<0)
+		exit(1);
+	
+	/*continued*/
+	/*write pipe*/
+	
+	write(p[1],msg1,MSGSIZE); //write(fildes,message,message size)
+	write(p[1],msg2,MSGSIZE);
+	write(p[1],msg3,MSGSIZE);
+	
+	for(i=0;i<3;i++){
+		/*read pipe*/
+		read(p[0], inbuf, MSGSIZE);//read(filedes,msg,msgsize)
+		printf("%s\n", inbuf);
+	}
+	return 0;
+}
