decf054491d0cf377f4fd62994a92567e80a8026
sahani02 <2002sahani@gmail.com>
2025-06-09 12:20:01 +0530

    Update d20_HW.c

--- DAY 20/d20_HW.c
+++ DAY 20/d20_HW.c
@@ -8,7 +8,7 @@
 
 // Structure for inputs
 typedef struct {
-    char shape[20];
+    int shapeCode;  // 1: Circle, 2: Rectangle, 3: Square, 4: Triangle
     float dim1, dim2;
 } ShapeInput;
 
@@ -34,7 +34,6 @@ int main() {
         perror("Fork failed");
         exit(1);
     }
-
     else if (pid == 0) {
         // Child Process
         close(pipefd[1]); // Close write end
@@ -43,19 +42,13 @@ int main() {
         read(pipefd[0], &input, sizeof(input));
         close(pipefd[0]);
 
-        int shapeCode = -1;
-        if (strcmp(input.shape, "circle") == 0) shapeCode = 1;
-        else if (strcmp(input.shape, "rectangle") == 0) shapeCode = 2;
-        else if (strcmp(input.shape, "square") == 0) shapeCode = 3;
-        else if (strcmp(input.shape, "triangle") == 0) shapeCode = 4;
-
         float area = 0;
-        switch (shapeCode) {
+        switch (input.shapeCode) {
             case 1: // Circle
                 area = 3.1416 * input.dim1 * input.dim1;
                 break;
             case 2: // Rectangle
-                area = input.dim1 * input.dim2;
+			 area = input.dim1 * input.dim2;
                 break;
             case 3: // Square
                 area = input.dim1 * input.dim1;
@@ -64,7 +57,7 @@ int main() {
                 area = 0.5 * input.dim1 * input.dim2;
                 break;
             default:
-                fprintf(stderr, "Invalid shape received.\n");
+                fprintf(stderr, "Invalid shape code received.\n");
                 exit(1);
         }
 
@@ -75,34 +68,62 @@ int main() {
 
         exit(0);
     }
-
     else {
         // Parent Process
         close(pipefd[0]); // Close read end
 
         ShapeInput input;
-        printf("Enter shape (circle, rectangle, square, triangle): ");
-        scanf("%s", input.shape);
-        printf("Enter dimension 1: ");
-        scanf("%f", &input.dim1);
+        printf("Area Calculation Program\n");
+        printf("Choose a shape:\n");
+        printf("1. Circle\n");
+        printf("2. Rectangle\n");
+        printf("3. Square\n");
+        printf("4. Triangle\n");
+        printf("Enter your choice (1-4): ");
+        scanf("%d", &input.shapeCode);
 
-        if (strcmp(input.shape, "circle") != 0 && strcmp(input.shape, "square") != 0) {
-            printf("Enter dimension 2: ");
-            scanf("%f", &input.dim2);
-        } else {
-            input.dim2 = 0;
-        }
+        switch (input.shapeCode) {
+            case 1:
+                printf("Enter radius: ");
+                scanf("%f", &input.dim1);
+                input.dim2 = 0;
+                break;
+            case 2:
+                printf("Enter length: ");
+                scanf("%f", &input.dim1);
+                printf("Enter width: ");
+                scanf("%f", &input.dim2);
+                break;
+             case 3:
+                printf("Enter side length: ");
+                scanf("%f", &input.dim1);
+                input.dim2 = 0;
+                break;
+            case 4:
+                printf("Enter base: ");
+                scanf("%f", &input.dim1);
+                printf("Enter height: ");
+                scanf("%f", &input.dim2);
+                break;
+            default:
+                printf("Invalid choice\n");
+                close(pipefd[1]);
+                shmctl(shmid, IPC_RMID, NULL);
+                exit(1);
+		}
 
-        write(pipefd[1], &input, sizeof(input));
-        close(pipefd[1]);
+			write(pipefd[1], &input, sizeof(input));
+			close(pipefd[1]);
 
-        wait(NULL); // Wait for child to finish
+			wait(NULL); // Wait for child to finish
 
-        float *shm_area = (float *)shmat(shmid, NULL, 0);
-        printf("Area of %s = %.2f\n", input.shape, *shm_area);
-        shmdt(shm_area);
-        shmctl(shmid, IPC_RMID, NULL); // Cleanup
-    }
+			float *shm_area = (float *)shmat(shmid, NULL, 0);
+			const char *shape_names[] = {"", "Circle", "Rectangle", "Square", "Triangle"};
+			printf("Area of %s = %.2f\n", shape_names[input.shapeCode], *shm_area);
+			shmdt(shm_area);
+			shmctl(shmid, IPC_RMID, NULL); // Cleanup
+	}
 
-    return 0;
+		return 0;
 }
+
