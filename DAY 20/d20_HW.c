3b8b0ee90d5704af9242d65a933772d80f188030
sahani02 <2002sahani@gmail.com>
2025-06-09 11:21:10 +0530

    Update d20_HW.c

--- DAY 20/d20_HW.c
+++ DAY 20/d20_HW.c
@@ -13,7 +13,7 @@ typedef struct {
 } ShapeInput;
 
 int main() {
-    int pipefd[2]; // pipe for parent -> child
+    int pipefd[2];
     pid_t pid;
 
     // Create shared memory
@@ -43,21 +43,36 @@ int main() {
         read(pipefd[0], &input, sizeof(input));
         close(pipefd[0]);
 
+        int shapeCode = -1;
+        if (strcmp(input.shape, "circle") == 0) shapeCode = 1;
+        else if (strcmp(input.shape, "rectangle") == 0) shapeCode = 2;
+        else if (strcmp(input.shape, "square") == 0) shapeCode = 3;
+        else if (strcmp(input.shape, "triangle") == 0) shapeCode = 4;
+
         float area = 0;
-        if (strcmp(input.shape, "circle") == 0) {
-            area = 3.1416 * input.dim1 * input.dim1;
-        } else if (strcmp(input.shape, "rectangle") == 0) {
-            area = input.dim1 * input.dim2;
-        } else if (strcmp(input.shape, "square") == 0) {
-            area = input.dim1 * input.dim1;
-        } else if (strcmp(input.shape, "triangle") == 0) {
-            area = 0.5 * input.dim1 * input.dim2;
+        switch (shapeCode) {
+            case 1: // Circle
+                area = 3.1416 * input.dim1 * input.dim1;
+                break;
+            case 2: // Rectangle
+                area = input.dim1 * input.dim2;
+                break;
+            case 3: // Square
+                area = input.dim1 * input.dim1;
+                break;
+            case 4: // Triangle
+                area = 0.5 * input.dim1 * input.dim2;
+                break;
+            default:
+                fprintf(stderr, "Invalid shape received.\n");
+                exit(1);
         }
 
-        // Write area to shared memory
+        // Write result to shared memory
         float *shm_area = (float *)shmat(shmid, NULL, 0);
         *shm_area = area;
         shmdt(shm_area);
+
         exit(0);
     }
 
@@ -70,6 +85,7 @@ int main() {
         scanf("%s", input.shape);
         printf("Enter dimension 1: ");
         scanf("%f", &input.dim1);
+
         if (strcmp(input.shape, "circle") != 0 && strcmp(input.shape, "square") != 0) {
             printf("Enter dimension 2: ");
             scanf("%f", &input.dim2);
@@ -80,12 +96,12 @@ int main() {
         write(pipefd[1], &input, sizeof(input));
         close(pipefd[1]);
 
-        wait(NULL); // Wait for child
+        wait(NULL); // Wait for child to finish
 
         float *shm_area = (float *)shmat(shmid, NULL, 0);
         printf("Area of %s = %.2f\n", input.shape, *shm_area);
         shmdt(shm_area);
-        shmctl(shmid, IPC_RMID, NULL); // Remove shared memory
+        shmctl(shmid, IPC_RMID, NULL); // Cleanup
     }
 
     return 0;
